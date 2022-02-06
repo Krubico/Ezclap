21720165624e5a24471d46cf0da70fb2e8fc607f
Kyrost <98030656+Kyrost@users.noreply.github.com>
2022-02-06 20:49:44 +0800

    Update Coding_Game.cpp

--- Coding_Game.cpp
+++ Coding_Game.cpp
@@ -669,8 +669,7 @@ void Displayscores()
 {
 	string username;
 	fstream inFile;
-	int points = 5;
-	username = "Kai Heng";
+	int points = 0;
 	inFile.open("scores.txt", ios::app);
 	if (inFile.is_open()) 
 	{
